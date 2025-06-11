a656ca9104c22a802258171dee26fcdb0eae4b3a
Syed Asim Shah <syedasimshah1994@gmail.com>
2025-06-11 14:43:37 -0400

    feat: added base functionality for quick todo
    Users can toggle a floating window to open a project
    specific markdown todo file. Users can add todos here,
    and have free form text.
    
    Current config lets users set window height, width,
    border and transparency. Users can also set the toggle
    key.

--- lua/quick-todo/config.lua
+++ lua/quick-todo/config.lua
@@ -1,16 +1,36 @@
+local commands = require("quick-todo.commands")
 ---@class Config
 local M = {}
 
----@class DefaultOptions
-M.defaults = { name = "John Doe" }
+---@type Options
+M.defaults = {
+  keys = {
+    open = "+",
+  },
+  window = {
+    height = 0.5,
+    width = 0.5,
+    border = "rounded",
+    winblend = 0,
+  },
+}
 
----@class Options
+---@type Options
 M.options = {}
 
 ---Extend the defaults options table with the user options
----@param opts UserOptions: plugin options
+---@param opts Options: plugin options
 M.setup = function(opts)
   M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
+
+  for subcmd, def in pairs(commands.sub_cmds) do
+    local key = M.options.keys[subcmd]
+    if key then
+      vim.keymap.set("n", key, function()
+        vim.cmd("QuickTodo " .. subcmd)
+      end, { desc = "QuickTodo: " .. def.desc })
+    end
+  end
 end
 
 return M
